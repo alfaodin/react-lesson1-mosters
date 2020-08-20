9c9bab488b8a8ef3bb7efda9174763e654219631
Christian Cartes <alfaodin@gmail.com>
2020-08-19 22:42:21 -0500

    @CCartes ejemplo de state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  const name = 'Christian Cartes';
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p className="hello-world">
-          HOLA MUNDO <span className="user-name">{name}</span>
-        </p>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  constructor() {
+    super();
+    this.state = {
+      name: 'Christian Cartes'
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p className="hello-world">
+            HOLA <span className="user-name">{this.state.name}</span>
+          </p>
+          <button onClick={() => this.setState({ name: 'Lur rey de Omicron' })}>
+            Saludos a Lur
+          </button>
+        </header>
+      </div>
+    );
+  }
+}
